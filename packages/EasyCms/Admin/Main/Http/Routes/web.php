505a6bf12559519a4365ccf6fd867707e19f40d8
Evgeny <autorun71@mail.ru>
2020-10-18 16:09:49 +0300

    add information route

--- packages/EasyCms/Admin/Main/Http/Routes/web.php
+++ packages/EasyCms/Admin/Main/Http/Routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Pkg\EasyCms\Admin\Main\Http\Controllers\MainController;
 use Pkg\EasyCms\Admin\Main\Http\Controllers\Settings\SettingsController;
 
-
-Route::get('/', [MainController::class, 'index'])->name('admin.information');
+Route::get('', function () {
+    return redirect(\route('admin.information'));
+});
+Route::get('/information', [MainController::class, 'index'])->name('admin.information');
 Route::get('/settings', [SettingsController::class, 'index'])->name('admin.settings');
 
 
